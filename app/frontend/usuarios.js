0d5cd9a215507cee32d0696dc4baf8b7d72e3c39
Gaspar Cavallero <gaspicava1510@gmail.com>
2025-06-30 15:29:30 -0300

    bugs arreglados

--- app/frontend/usuarios.js
+++ app/frontend/usuarios.js
@@ -189,37 +189,32 @@ function cargarHistorial() {
 }
 
 
-const headers = { Authorization: `Bearer ${token}` };
+function cargarDashboard() {
+  const headers = { Authorization: `Bearer ${token}` };
 
-fetch("http://localhost:8000/dashboard/total-eventos", { headers })
-  .then(res => res.json())
-  .then(data => document.getElementById("totalEventos").textContent = data.total_eventos);
+  fetch("http://localhost:8000/dashboard/total-eventos", { headers })
+    .then(res => res.json())
+    .then(data => document.getElementById("totalEventos").textContent = data.total_eventos);
 
-fetch("http://localhost:8000/dashboard/inscripciones-activas", { headers })
-  .then(res => res.json())
-  .then(data => document.getElementById("inscripcionesActivasNav").textContent = data.inscripciones_activas);
-
-fetch("http://localhost:8000/dashboard/promedio-inscriptos", { headers })
-  .then(res => res.json())
-  .then(data => document.getElementById("promedio").textContent = data.promedio_inscriptos_por_evento);
-
-fetch("http://localhost:8000/dashboard/evento-mas-inscripciones", { headers })
-  .then(res => res.json())
-  .then(data => {
-    if (data.nombre) {
-      document.getElementById("masPopular").textContent = data.nombre;
-      document.getElementById("cantPopular").textContent = data.inscripciones;
-    } else {
-      document.getElementById("masPopular").textContent = "Ninguno";
-      document.getElementById("cantPopular").textContent = "0";
-    }
-  });
+  fetch("http://localhost:8000/dashboard/evento-mas-inscripciones", { headers })
+    .then(res => res.json())
+    .then(data => {
+      if (data.nombre) {
+        document.getElementById("masPopular").textContent = data.nombre;
+        document.getElementById("cantPopular").textContent = data.inscripciones;
+      } else {
+        document.getElementById("masPopular").textContent = "Ninguno";
+        document.getElementById("cantPopular").textContent = "0";
+      }
+    });
+}
 
 // Refrescar UI completa
 function refrescarUI() {
   cargarInscripcionesActivas();
   cargarEventos();
   cargarHistorial();
+  cargarDashboard();
 }
 
 // Inicializar al entrar
